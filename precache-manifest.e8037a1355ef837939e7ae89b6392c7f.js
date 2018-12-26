a8166a6c2874cbe8299f6c57909add1f6bbd38f1
Madhava Di Florio <mattdiflorio@gmail.com>
2018-12-26 12:46:39 +0100

    Updates

---
+++ precache-manifest.e8037a1355ef837939e7ae89b6392c7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "564805684e06adbc1ec6",
+    "url": "/static/js/main.56480568.chunk.js"
+  },
+  {
+    "revision": "964a1169fbcd67efb748",
+    "url": "/static/js/1.964a1169.chunk.js"
+  },
+  {
+    "revision": "564805684e06adbc1ec6",
+    "url": "/static/css/main.9ce26594.chunk.css"
+  },
+  {
+    "revision": "42bf958bde067850ff806aeac64a1836",
+    "url": "/index.html"
+  }
+];
